08338566b12a63e2d9a35e61228759ea2f6dc5bc
Masud-Rana-1002 <masudrana19981002@gmail.com>
2025-01-24 09:52:47 +0300

    add

--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,10 @@ export default async function    Home()  {
       <div>
         
         <h1 className='text-3xl font-bold text-center p-4'>Blog Posts</h1>
-       <div className='grid grid-cols-4 gap-6 '>
+       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 '>
           {postData.map((post)=>(
             <Link href={`/blog/${post.id}`} key={post.id}>
-              <div className='bg-white shadow-lg rounded-lg
+              <div className='bg-white  shadow-lg rounded-lg
              p-1  hover:scale-105 transition-all ease-in-out duration-300 hover:shadow-2xl h-20 hover:cursor-pointer ' key={post.id}>
               <h2 className='text-lg font-medium p-1'>{post.title}</h2>
               
